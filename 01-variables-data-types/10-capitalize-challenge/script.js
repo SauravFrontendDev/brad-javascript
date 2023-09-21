f2ad81c28bb3a873c6b8e4aa1dd79a4ad41874f1
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-09-21 21:49:55 +0530

    section 1 completed

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,14 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
-const myString = 'developer';
+const myString = "developer";
+
+const result = myString.charAt(0).toUpperCase() + myString.slice(1);
+
+console.log(result);
+
+const result2 = myString.at(0).toUpperCase() + myString.substring(1);
+
+console.log(result2);
+
+const result3 = `${myString.at(0).toUpperCase()}${myString.slice(1)}`;
+
+console.log(result3);
---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,16 @@
+const x = Math.ceil(Math.random() * 100);
+const y = Math.ceil(Math.random() * 50);
+
+console.log(x, y);
+
+const sum = x + y;
+const difference = x - y;
+const product = x * y;
+const quotiennt = x / y;
+const remainder = x % y;
+
+console.log(`${x} + ${y} = ${sum}`);
+console.log(`${x} - ${y} = ${difference}`);
+console.log(`${x} * ${y} = ${product}`);
+console.log(`${x} / ${y} = ${quotiennt}`);
+console.log(`${x} % ${y} = ${remainder}`);
